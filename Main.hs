ec5e20ac908a3741237ad3a0d3590309cdbc405b
Sjoerd Visscher <sjoerd@w3future.com>
2009-10-08 00:39:57 +0200

    Added an example using attributes

--- Main.hs
+++ Main.hs
@@ -5,3 +5,6 @@ main = runFile "test.moi" >>= putStr . showResult
 
 r :: String -> IO ()
 r = putStr . showResult . runString
+
+f :: String -> IO ()
+f n = runFile (n ++ ".moi") >>= putStr . showResult
