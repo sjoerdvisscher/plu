d3525ca87f6f39e3f08def6a2a71ad4f56f39f1c
Sjoerd Visscher <sjoerd@w3future.com>
2009-11-03 17:52:27 +0100

    Created examples dir. Added a set example.

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ import Moiell
 import Moiell.MonadInstance
 import Moiell.Semantics
 import Moiell.CPS
+import Moiell.JS
 import Moiell.Serialize
 
 main :: IO ()
@@ -11,16 +12,22 @@ r :: String -> IO ()
 r s = putStr $ run (compileString s :: M Comp)
 
 f :: String -> IO ()
-f n = (compileFile (n ++ ".moi") :: IO (M Comp)) >>= putStr . run
+f n = (compileFile ("examples/" ++ n ++ ".moi") :: IO (M Comp)) >>= putStr . run
 
 cr :: String -> IO ()
 cr s = putStr $ run (compileString s :: M CPS)
 
 cf :: String -> IO ()
-cf n = (compileFile (n ++ ".moi") :: IO (M CPS)) >>= putStr . run
+cf n = (compileFile ("examples/" ++ n ++ ".moi") :: IO (M CPS)) >>= putStr . run
+
+jr :: String -> IO ()
+jr s = putStrLn $ run (compileString s :: JS)
+
+jf :: String -> IO ()
+jf n = (compileFile ("examples/" ++ n ++ ".moi") :: IO JS) >>= putStrLn . run
 
 dr :: String -> IO ()
 dr s = putStrLn $ run (compileString s :: Src)
 
 df :: String -> IO ()
-df n = (compileFile (n ++ ".moi") :: IO Src) >>= putStrLn . run
+df n = (compileFile ("examples/" ++ n ++ ".moi") :: IO Src) >>= putStrLn . run
